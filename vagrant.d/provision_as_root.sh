d06279ca82544a4297c60461e3d616a0cb9cacd3
Lain-dono <lain.dono@gmail.com>
2016-06-06 23:25:55 +0300

    fix captha rendering on vagrant

--- vagrant.d/provision_as_root.sh
+++ vagrant.d/provision_as_root.sh
@@ -12,6 +12,14 @@ sed -i 's/# ru_RU\.UTF-8 UTF-8/ru_RU\.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
 
 if which apt-get > /dev/null; then
+  cat << EOF >> /etc/apt/sources.list
+deb http://httpredir.debian.org/debian jessie main contrib
+deb-src http://httpredir.debian.org/debian jessie main contrib
+
+deb http://security.debian.org/ jessie/updates main contrib
+deb-src http://security.debian.org/ jessie/updates main contrib
+EOF
+
   # install some debian packages
   echo "mysql-server mysql-server/root_password password $DBPASSWD" | debconf-set-selections
   echo "mysql-server mysql-server/root_password_again password $DBPASSWD" | debconf-set-selections
